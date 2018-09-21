763033dd1788db7ff29b2cee721893f894c25161
papic <danilo.p@vivifyideas.com>
2018-09-21 19:16:22 +0200

    Initial project set up, config and deploy script set.

---
+++ .vuepress/config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    base: '/style-guide/',
+    title: 'Vivify Ideas Style Guides',
+    description: 'Vivify Ideas coding style guides for various tech stacks'
+};
---
+++ deploy.sh
@@ -0,0 +1,21 @@
+# abort on errors
+set -e
+
+# build
+npm run build
+
+# navigate into the deploy directory
+cd .vuepress/dist
+
+# if you are deploying to a custom domain
+# echo 'www.example.com' > CNAME
+
+git init
+git checkout -b gh-pages
+git add -A
+git commit -m 'deploy'
+
+# if you are deploying to https://<USERNAME>.github.io/<REPO>
+git push -f https://github.com/Vivify-Ideas/style-guide.git gh-pages
+
+cd -
